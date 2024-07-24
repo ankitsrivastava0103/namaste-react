6fa3a8b55b139f3020f22101136c712cefd95028
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2024-07-24 08:24:11 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,34 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: "child1" }, [
-    React.createElement("h1", { key: "h1" }, "Hello From Nested H1 Tag"),
-    React.createElement(
-      "h2",
-      { key: "h2" },
-      "Hello From Nested Siblling H2 Tag"
-    ),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", { key: "h1" }, "Hello From Nested CHild H1 Tag"),
-    React.createElement(
-      "h2",
-      { key: "h2" },
-      "Hello From Nested CHild Siblling H2 Tag"
-    ),
-  ]),
-]);
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
+const title = <h1 className="head">Namaste React Title Component</h1>;
+// React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    {title}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-root.render(parent);
-
-console.log(parent);
+root.render(<HeadingComponent />);
