65f483c82da078020291b1d4737e8a1bb6ddbea2
Ankit Srivastava <ankitsrivastava@Ankits-MacBook-Pro.local>
2025-03-26 01:18:50 +0530

    Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,70 @@
-import ResturantCard from "./ResturandCard";
 import RES_DATA from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import ResturantCard from "./ResturandCard";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [resData, setResData] = useState(RES_DATA);
+  const [resData, setResData] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const handleFilterClick = () => {
     const filteredData = resData.filter((res) => {
       return res.rating >= 4.5;
     });
-    setResData(filteredData);
+    setFiltered(filteredData);
   };
+
+  const handleSearchClick = () => {
+    const filteredData = resData.filter((res) => {
+      const name = res.name.toLowerCase();
+      if (name.includes(searchValue.toLowerCase())) {
+        return res;
+      }
+    });
+    setFiltered(filteredData);
+  };
+
+  const handleInputChange = () => {
+    setSearchValue(event.target.value);
+  };
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9615398&lng=79.2961468&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // setResData(json) Commented as using mock data
+    setResData(RES_DATA);
+    setFiltered(RES_DATA);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (resData.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            onChange={handleInputChange}
+            value={searchValue}
+          />
+          <button onClick={handleSearchClick}>Search</button>
+        </div>
         <button className="filtter-button" onClick={handleFilterClick}>
           Top Rated Resturants
         </button>
       </div>
       <div className="resturant-container">
-        {resData?.map((resturant) => {
+        {filtered?.map((resturant) => {
           return <ResturantCard key={resturant.id} resturantData={resturant} />;
         })}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
+  const handleButtonClick = () => {
+    if (btnName === "Login") {
+      setBtnName("Logout");
+    } else {
+      setBtnName("Login");
+    }
+  };
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +23,9 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="login" onClick={handleButtonClick}>
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
